d01e3be840ddd9eaf49782125577755ebacf1a2d
Jonathan 'theJPster' Pallant <git@thejpster.org.uk>
2016-03-22 10:51:55 +0000

    Support wrapped headers.

--- src/http_parser.rs
+++ src/http_parser.rs
@@ -12,22 +12,12 @@ pub enum HttpMethod {
     HEAD,
 }
 
-#[derive(Debug, PartialEq)]
-pub enum HttpHeader {
-    // More common header types here. Is this useful?
-    // CharSet(String),
-    Unknown {
-        key: String,
-        value: String,
-    },
-}
-
 #[derive(Debug)]
 pub struct HttpRequest {
     pub url: String,
     pub method: HttpMethod,
     pub protocol: String,
-    pub headers: Vec<HttpHeader>,
+    pub headers: Vec<(String, String)>,
 }
 
 /// Contains the internal state for the parser. Must be given
@@ -45,7 +35,7 @@ pub struct ParseContext {
     /// The protocol in the request
     protocol: String,
     /// A collection of HTTP headers (key,value) pairs
-    headers: Vec<HttpHeader>,
+    headers: Vec<(String, String)>,
     /// A temporary holder for the key while we read the value
     key: String,
 }
@@ -69,6 +59,9 @@ enum ParseState {
     VersionEOL,
     KeyStart,
     Key,
+    WrappedValue,
+    WrappedValueStart,
+    WrappedValueEOL,
     ValueStart,
     Value,
     ValueEOL,
@@ -107,7 +100,6 @@ impl ParseContext {
         for b in buffer {
             let c = *b;
             let ct = get_char_type(c);
-            println!("Got char type {:?} in state {:?}", ct, self.state);
             // switch on state, then switch on char type
             match self.state {
                 ParseState::Method => {
@@ -116,14 +108,14 @@ impl ParseContext {
                         CharType::Space => {
                             match String::from_utf8(self.temp.split_off(0)) {
                                 Ok(s) => {
-                                    match s.as_str() {
-                                        "GET" => self.method = HttpMethod::GET,
-                                        "POST" => self.method = HttpMethod::POST,
-                                        "PUT" => self.method = HttpMethod::PUT,
-                                        "OPTION" => self.method = HttpMethod::OPTION,
-                                        "HEAD" => self.method = HttpMethod::HEAD,
+                                    self.method = match s.as_str() {
+                                        "GET" => HttpMethod::GET,
+                                        "POST" => HttpMethod::POST,
+                                        "PUT" => HttpMethod::PUT,
+                                        "OPTION" => HttpMethod::OPTION,
+                                        "HEAD" => HttpMethod::HEAD,
                                         _ => return ParseResult::Error,
-                                    }
+                                    };
                                     println!("Got method {:?}", self.method)
                                 }
                                 _ => return ParseResult::Error,
@@ -169,12 +161,13 @@ impl ParseContext {
                 }
                 ParseState::KeyStart => {
                     match ct {
+                        CharType::Space => self.state = ParseState::WrappedValueStart,
                         CharType::CR => self.state = ParseState::FinalEOL,
                         CharType::Other => {
                             self.temp.push(c);
                             self.state = ParseState::Key
                         }
-                        _ => return ParseResult::Error,
+                        _ => return ParseResult::Error
                     }
                 }
                 ParseState::Key => {
@@ -206,10 +199,7 @@ impl ParseContext {
                         CharType::CR => {
                             match String::from_utf8(self.temp.split_off(0)) {
                                 Ok(s) => {
-                                    let hdr: HttpHeader = HttpHeader::Unknown {
-                                        key: self.key.clone(),
-                                        value: s,
-                                    };
+                                    let hdr = (self.key.clone(), s);
                                     println!("Got header {:?}", hdr);
                                     self.headers.push(hdr);
                                 }
@@ -221,6 +211,45 @@ impl ParseContext {
                     }
                 }
                 ParseState::ValueEOL => {
+                    match ct {
+                        CharType::NL => self.state = ParseState::KeyStart,
+                        _ => return ParseResult::Error,
+                    }
+                }
+                ParseState::WrappedValueStart => {
+                    match ct {
+                        CharType::Space => { },
+                        CharType::Other | CharType::Colon => {
+                            self.temp.push(0x20); // single space
+                            self.temp.push(c);
+                            self.state = ParseState::WrappedValue
+                        }
+                        CharType::CR => self.state = ParseState::WrappedValueEOL,
+                        _ => return ParseResult::Error
+                    }
+                }
+                ParseState::WrappedValue => {
+                    match ct {
+                        CharType::Other | CharType::Colon | CharType::Space => {
+                            self.temp.push(c)
+                        }
+                        CharType::CR => {
+                            match String::from_utf8(self.temp.split_off(0)) {
+                                Ok(s) => {
+                                    match self.headers.last_mut() {
+                                        Some(x) => x.1.push_str(s.as_str()),
+                                        None => return ParseResult::Error
+                                    }
+                                    println!("Appended {:?}", s);
+                                },
+                                _ => return ParseResult::Error
+                            }
+                            self.state = ParseState::WrappedValueEOL
+                        },
+                        _ => return ParseResult::Error
+                    }
+                }
+                ParseState::WrappedValueEOL => {
                     match ct {
                         CharType::NL => self.state = ParseState::KeyStart,
                         _ => return ParseResult::Error,
@@ -262,7 +291,7 @@ impl Default for ParseState {
 /// Map an octet (in US-ASCII) to a character
 /// class, so we can decide what to do with it.
 fn get_char_type(b: u8) -> CharType {
-    if b == 0x20 {
+    if (b == 0x20) || (b == 0x09) {
         CharType::Space
     } else if b == 0x0D {
         CharType::CR
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -15,16 +15,26 @@ fn get_complete_header() {
             assert_eq!(r.url, "/index.html");
             assert_eq!(r.protocol, "HTTP/1.1");
             assert_eq!(r.headers.len(), 2);
-            assert_eq!(r.headers[0],
-                       HttpHeader::Unknown {
-                           key: String::from("User-Agent"),
-                           value: String::from("rust test"),
-                       });
-            assert_eq!(r.headers[1],
-                       HttpHeader::Unknown {
-                           key: String::from("Host"),
-                           value: String::from("localhost"),
-                       });
+            assert_eq!(r.headers[0], (String::from("User-Agent"), String::from("rust test")));
+            assert_eq!(r.headers[1], (String::from("Host"), String::from("localhost")));
+        }
+        _ => assert!(false),
+    }
+}
+
+#[test]
+fn get_complete_wrapped_header() {
+    let mut ctx: ParseContext = Default::default();
+    let test = "GET /index.html HTTP/1.1\r\nUser-Agent: rust test\r\n\t\tis the best test\r\nHost: localhost\r\n\r\n"
+                   .as_bytes();
+    match ctx.parse_header(test) {
+        ParseResult::Complete(r) => {
+            assert_eq!(r.method, HttpMethod::GET);
+            assert_eq!(r.url, "/index.html");
+            assert_eq!(r.protocol, "HTTP/1.1");
+            assert_eq!(r.headers.len(), 2);
+            assert_eq!(r.headers[0], (String::from("User-Agent"), String::from("rust test is the best test")));
+            assert_eq!(r.headers[1], (String::from("Host"), String::from("localhost")));
         }
         _ => assert!(false),
     }
@@ -42,16 +52,8 @@ fn put_complete_header() {
             assert_eq!(r.url, "/v1/api/frob?foo=bar");
             assert_eq!(r.protocol, "HTTP/1.0");
             assert_eq!(r.headers.len(), 2);
-            assert_eq!(r.headers[0],
-                       HttpHeader::Unknown {
-                           key: String::from("User-Agent"),
-                           value: String::from("rust test"),
-                       });
-            assert_eq!(r.headers[1],
-                       HttpHeader::Unknown {
-                           key: String::from("Host"),
-                           value: String::from("localhost"),
-                       });
+            assert_eq!(r.headers[0], (String::from("User-Agent"), String::from("rust test")));
+            assert_eq!(r.headers[1], (String::from("Host"), String::from("localhost")));
         }
         _ => assert!(false),
     }
