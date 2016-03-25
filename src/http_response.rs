98b607e09607e6ad89a75b22ee51bbb7f15645fa
theJPster <github@thejpster.org.uk>
2016-03-25 21:17:06 +0000

    Added HTTP response status codes.

--- src/http_response.rs
+++ src/http_response.rs
@@ -11,20 +11,83 @@
 
 use std::collections::HashMap;
 
-//use http::*;
-
 // ****************************************************************************
 //
 // Public Types
 //
 // ****************************************************************************
 
+#[derive(Debug)]
+pub enum HttpResponseStatus {
+    Continue = 100,
+    SwitchingProtocols = 101,
+    Processing = 102,
+    Ok = 200,
+    Created = 201,
+    Accepted = 202,
+    NonAuthoritativeInformation = 203,
+    NoContent = 204,
+    ResetContent = 205,
+    PartialContent = 206,
+    MultiStatus = 207,
+    AlreadyReported = 208,
+    ImUsed = 226,
+    MultipleChoices = 300,
+    MovedPermanently = 301,
+    Found = 302,
+    SeeOther = 303,
+    NotModified = 304,
+    UseProxy = 305,
+    SwitchProxy = 306,
+    TemporaryRedirect = 307,
+    PermanentRedirect = 308,
+    BadRequest = 400,
+    Unauthorized = 401,
+    PaymentRequired = 402,
+    Forbidden = 403,
+    NotFound = 404,
+    MethodNotAllowed = 405,
+    NotAcceptable = 406,
+    ProxyAuthenticationRequired = 407,
+    RequestTimeout = 408,
+    Conflict = 409,
+    Gone = 410,
+    LengthRequired = 411,
+    PreconditionFailed = 412,
+    PayloadTooLarge = 413,
+    URITooLong = 414,
+    UnsupportedMediaType = 415,
+    RangeNotSatisfiable = 416,
+    ExpectationFailed = 417,
+    IAmATeapot = 418,
+    MisdirectedRequest = 421,
+    UnprocessableEntity = 422,
+    Locked = 423,
+    FailedDependency = 424,
+    UpgradeRequired = 426,
+    PreconditionRequired = 428,
+    TooManyRequests = 429,
+    RequestHeaderFieldsTooLarge = 431,
+    UnavailableForLegalReasons = 451,
+    InternalServerError = 500,
+    NotImplemented = 501,
+    BadGateway = 502,
+    ServiceUnavailable = 503,
+    GatewayTimeout = 504,
+    HTTPVersionNotSupported = 505,
+    VariantAlsoNegotiates = 506,
+    InsufficientStorage = 507,
+    LoopDetected = 508,
+    NotExtended = 510,
+    NetworkAuthenticationRequired = 511,
+}
+
 /// An HTTP Response.
 /// Fully describes the HTTP response sent from the server to the client.
 #[derive(Debug)]
 pub struct HttpResponse {
     /// The HTTP result code - @todo should be an enum
-    pub result: u32,
+    pub status: HttpResponseStatus,
     /// The protocol the client is using in the response
     pub protocol: String,
     /// Any headers supplied by the server in the response
